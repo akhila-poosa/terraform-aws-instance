518236c15e133304e6d7f3f9ef3bd8160eef1460
akhilapoosaa <akhila.1829@gmail.com>
2025-10-31 13:09:55 +0530

    updated

--- ec2.tf
+++ ec2.tf
@@ -1,1 +1,6 @@
-#
+resource "aws_instance" "this" {
+    ami = var.ami_id # mandatory to provide ami_id
+    instance_type = var.instance_type 
+    vpc_security_group_ids = var.sg_ids 
+    tags = var.tags # optional if needed we must provide
+}
